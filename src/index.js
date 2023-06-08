d33ff5479b841f21356b02ad007b3dcff7feec8b
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2023-06-08 15:04:34 +0300

    Delete index.js

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import KiwiEmitter from "./dist/Emitter.js";
-import * as __KiwiEmitterError from "./dist/EmitterError.js";
-const KiwiEmitterError = __KiwiEmitterError.default;
-
-export { KiwiEmitterError };
-export default KiwiEmitter;
